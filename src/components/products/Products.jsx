2e091c9c91ff52388c384f7ef8e202af3c7a892b
Sofia Genchi <sofigen16@gmail.com>
2024-03-07 15:59:44 -0300

    caja de productos

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -9,17 +9,30 @@ function Products() {
         <div>
             {data.productos.map(product => (
                 <div class="card">
-                <div class="card-img"><div class="img"></div></div>
-                <div class="card-title">{product.nombre}</div>
-                <div class="card-subtitle">{product.descripcion}</div>
-                <hr class="card-divider" />
-                <div class="card-footer">
-                    <div class="card-price"><span>$</span> {product.precio}</div>
-                    <button class="card-btn">
-                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="m397.78 316h-205.13a15 15 0 0 1 -14.65-11.67l-34.54-150.48a15 15 0 0 1 14.62-18.36h274.27a15 15 0 0 1 14.65 18.36l-34.6 150.48a15 15 0 0 1 -14.62 11.67zm-193.19-30h181.25l27.67-120.48h-236.6z"></path><path d="m222 450a57.48 57.48 0 1 1 57.48-57.48 57.54 57.54 0 0 1 -57.48 57.48zm0-84.95a27.48 27.48 0 1 0 27.48 27.47 27.5 27.5 0 0 0 -27.48-27.47z"></path><path d="m368.42 450a57.48 57.48 0 1 1 57.48-57.48 57.54 57.54 0 0 1 -57.48 57.48zm0-84.95a27.48 27.48 0 1 0 27.48 27.47 27.5 27.5 0 0 0 -27.48-27.47z"></path><path d="m158.08 165.49a15 15 0 0 1 -14.23-10.26l-25.71-77.23h-47.44a15 15 0 1 1 0-30h58.3a15 15 0 0 1 14.23 10.26l29.13 87.49a15 15 0 0 1 -14.23 19.74z"></path></svg>
-                    </button>
+                <div class="rectangle left-top"></div>
+                <div class="rectangle right-top"></div>
+                <div class="rectangle left-bottom"></div>
+                <div class="rectangle right-bottom"></div>
+                <div class="image">
+                  <img src={product.imagen} alt="" />
                 </div>
+                <div class="love">
+                  <svg viewBox="0 0 512 512" xmlns={product.imagen} class="svg">
+                    <path
+                      d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"
+                    ></path>
+                  </svg>
+                  <span>{product.precio}</span>
                 </div>
+                <div class="category">
+                  <button class="button">{product.nombre}</button>
+                  <button class="button">{product.estado}</button>
+                </div>
+                <button class="action">
+                  <a className='linkWspp' href="http://" target="_blank" rel="noopener noreferrer">Lo quiero!</a>
+                </button>
+              </div>
+              
             ))}
         </div>
     </div>
